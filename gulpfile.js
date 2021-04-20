53218c95686e0b82daa3def7e32fbe84d10b072e
michal-simon <michal.simon@centrum.cz>
2021-04-20 14:13:00 +0200

    + compiled JS

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,6 @@
 /* eslint-disable no-console */
 
+const browserSync = require('browser-sync').create();
 const gulp = require('gulp');
 const gulpSass = require('gulp-sass');
 const sassVars = require('gulp-sass-vars');
@@ -7,17 +8,21 @@ const postcss = require('gulp-postcss');
 const autoprefixer = require('autoprefixer');
 const cssnano = require('cssnano');
 const rename = require('gulp-rename');
+const named = require('vinyl-named');
 const gulpHologram = require('gulp-hologram');
 const gulpHtmlmin = require('gulp-htmlmin');
 const gulpNunjucks = require('gulp-nunjucks');
 const yargs = require('yargs');
-const browserSync = require('browser-sync').create();
 const template = require('gulp-template');
+const webpackCompiler = require('webpack');
+const webpack = require('webpack-stream');
 const zip = require('gulp-zip');
 
 let port = 8000;
 let development = false;
 const appVersion = (typeof (yargs.argv.appVersion) !== 'undefined') ? yargs.argv.appVersion : 'dev';
+
+let webpackConfig = require('./webpack.config.js');
 
 const sass = () => {
 	return gulp.src('scss/style-*.scss')
@@ -74,6 +79,25 @@ const htmlmin = () => {
 
 const html = gulp.series(nunjucks, htmlmin);
 
+const transpile = () => {
+	let myConfig = {...webpackConfig};
+
+	if (!development) {
+		myConfig.optimization.minimize = true;
+	}
+
+	return gulp.src(['scripts/app-*.js'])
+		.pipe(named())
+		.pipe(webpack(myConfig, webpackCompiler))
+		.on('error', (err) => {
+			console.log(err.toString());
+		})
+		.pipe(gulp.dest('www/js'))
+		.pipe(browserSync.stream());
+};
+
+const js = transpile;
+
 // Documentation
 const hologram = () => {
 	return gulp.src('docs/hologram/config.yml')
@@ -95,8 +119,9 @@ const server = () => {
 };
 
 const watch = (callback) => {
-	gulp.watch(['scss/**/*.scss'], {interval: 500}, gulp.parallel(css/*, hologram*/));
+	gulp.watch(['scss/**/*.scss'], {interval: 500}, gulp.parallel(css, hologram));
 	gulp.watch(['html/**/*.njk'], {interval: 500}, html);
+	gulp.watch(['scripts/**/*.js'], {interval: 500}, js);
 
 	callback();
 };
@@ -113,7 +138,7 @@ const buildZip = () => {
 };
 
 
-const production = gulp.series(gulp.parallel(css, html)/*, hologram*/);
+const production = gulp.series(gulp.parallel(css, js, html), hologram);
 
 const defaultTask = gulp.series(setDevelopmentEnvironment, production, gulp.parallel(watch, server));
 
--- scripts/app-main.js
+++ scripts/app-main.js
@@ -1,13 +1,13 @@
 var togglerElement = document.getElementById("main-menu-toggler");
 var menuElement = document.getElementById("main-menu");
 var clickHandler = function (event) {
-    event.preventDefault();
+	event.preventDefault();
 
-    if (menuElement.classList.contains("show")) {
-        menuElement.classList.remove("show");
-    } else {
-        menuElement.classList.add("show");
-    }
+	if (menuElement.classList.contains("show")) {
+		menuElement.classList.remove("show");
+	} else {
+		menuElement.classList.add("show");
+	}
 };
 
 togglerElement.addEventListener("click", clickHandler);
