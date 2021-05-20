52219d21ea6e97fce67ca9cc13e20c11b47e4839
Jiri Tichy <jiri.tichy@codeac.io>
2021-05-20 17:48:13 +0200

    + menu handling dirty version

--- scripts/components/menu.js
+++ scripts/components/menu.js
@@ -12,4 +12,15 @@ export function menuToggle() {
 	};
 
 	togglerElement.addEventListener("click", clickHandler);
+
+	let menuItemElement1 = document.getElementById("menu-1");
+	let menuItemElement2 = document.getElementById("menu-2");
+	let menuItemElement3 = document.getElementById("menu-3");
+	let menuCloseHandler = function () {
+		menuElement.classList.remove("show");
+	};
+
+	menuItemElement1.addEventListener("click", menuCloseHandler);
+	menuItemElement2.addEventListener("click", menuCloseHandler);
+	menuItemElement3.addEventListener("click", menuCloseHandler);
 }
